7d1545467757f87b65bab52f165c5d4c007a1307
The Stig <TheStig294@users.noreply.github.com>
2021-06-13 14:49:14 +0930

    Harmless cleanup and added comments

--- lua/randomat2/cl_events/cl_yogsprophunt.lua
+++ lua/randomat2/cl_events/cl_yogsprophunt.lua
@@ -1,21 +1,24 @@
+-- Creates the prop hunt popup
 net.Receive("YogsPropHuntRandomat", function()
-	yogsprophuntpopup = vgui.Create("DFrame")
-	local xSize = 712
-	local ySize = 178
-    local pos1 = (ScrW()-xSize)/2
-    local pos2 = (ScrH()-ySize)/2
-    yogsprophuntpopup:SetPos(pos1,pos2)
-    yogsprophuntpopup:SetSize(xSize,ySize)
+    yogsprophuntpopup = vgui.Create("DFrame")
+    local xSize = 712
+    local ySize = 178
+    -- Places the frame at the centre of the screen
+    local pos1 = (ScrW() - xSize) / 2
+    local pos2 = (ScrH() - ySize) / 2
+    yogsprophuntpopup:SetPos(pos1, pos2)
+    yogsprophuntpopup:SetSize(xSize, ySize)
     yogsprophuntpopup:ShowCloseButton(false)
     yogsprophuntpopup:MakePopup()
-	yogsprophuntpopup.Paint = function(self, w, h) end
-	
-	local image = vgui.Create("DImage", yogsprophuntpopup)
+    yogsprophuntpopup.Paint = function(self, w, h) end
+    -- Places the popup image in the centre of the frame
+    local image = vgui.Create("DImage", yogsprophuntpopup)
     image:SetImage("materials/vgui/ttt/yogsprophunt/yogsprophunt.png")
-    image:SetPos(0,0)
-    image:SetSize(xSize,ySize)   
+    image:SetPos(0, 0)
+    image:SetSize(xSize, ySize)
 end)
 
+-- Closes the popup once time is up
 net.Receive("YogsPropHuntRandomatEnd", function()
-	yogsprophuntpopup:Close()
+    yogsprophuntpopup:Close()
 end)

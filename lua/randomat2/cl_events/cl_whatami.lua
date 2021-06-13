4a9076a7cd5a0fe8e34b931935a0418903874ae6
The Stig <TheStig294@users.noreply.github.com>
2021-06-13 14:47:58 +0930

    Cleanup and added comments

--- lua/randomat2/cl_events/cl_whatami.lua
+++ lua/randomat2/cl_events/cl_whatami.lua
@@ -1,39 +1,50 @@
+-- Default role hint popup time is 17 seconds
 local popupTime = 17
 
+-- Sent whenever this randomat tries to trigger
+-- Checks if the 'Hide role' option exists
+-- If not this randomat does not trigger
 net.Receive("WhatAmIRandomatConVarCheck", function()
+    -- If Malivil's 'Hide role' setting is detected,
     if ConVarExists("ttt_hide_role") then
+        -- Then tell the server this randomat can trigger,
         net.Start("WhatAmIRandomatCondition")
         net.WriteBool(true)
         net.SendToServer()
     else
+        -- Otherwise it cannot
         net.Start("WhatAmIRandomatCondition")
         net.WriteBool(false)
         net.SendToServer()
     end
 end)
 
+-- Randomat start
 net.Receive("WhatAmIRandomat", function()
+    -- Get the role hint popup time and turn it off
     popupTime = GetConVar("ttt_startpopup_duration"):GetInt()
     GetConVar("ttt_startpopup_duration"):SetInt(0)
+    -- Hide the player's role
     GetConVar("ttt_hide_role"):SetBool(true)
 
+    -- Prevent them from opening the scoreboard
     hook.Add("PlayerBindPress", "WhoAmIDisableScorboard", function(ply, bind, pressed)
         if (string.find(bind, "+showscores")) then return true end
     end)
 
-    local hud = {
-        ["TTTPickupHistory"] = true,
-        ["TTTPickupHistory"] = true
-    }
-
+    -- Hides the weapon pickup popup on the side as it's colour gives away your role
     hook.Add("HUDShouldDraw", "WhoAmIDisableWeaponHistory", function(name)
         if name == "TTTPickupHistory" then return false end
     end)
 end)
 
+-- Randomat end
 net.Receive("WhatAmIRandomatEnd", function()
+    -- Resets the role hint popup time
     GetConVar("ttt_startpopup_duration"):SetInt(popupTime)
+    -- Un-hides the player's role
     GetConVar("ttt_hide_role"):SetBool(false)
+    -- Re-enables the scoreboard and weapon pickup popups
     hook.Remove("PlayerBindPress", "WhoAmIDisableScorboard")
     hook.Remove("HUDShouldDraw", "WhoAmIDisableWeaponHistory")
 end)

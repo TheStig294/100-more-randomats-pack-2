bfe25c05b6567702b357ffa605593c738517b516
The Stig <TheStig294@users.noreply.github.com>
2021-06-14 12:57:24 +0930

    Removed old TTT2 support and cleanup

--- lua/randomat2/events/beback.lua
+++ lua/randomat2/events/beback.lua
@@ -4,20 +4,12 @@ EVENT.Description = "Turns everyone but traitors into phantoms"
 EVENT.id = "beback"
 
 function EVENT:Begin()
-    if TTT2 then
-        for k, ply in pairs(self:GetAlivePlayers(true)) do
-            if (ply:GetTeam() ~= TEAM_TRAITOR) then
-                ply:SetRole(ROLE_SPECTRE)
-            end
-        end
-    else
-        -- If not TTT2, for all alive players,
-        for k, ply in pairs(self:GetAlivePlayers(true)) do
-            -- That aren't traitors
-            if not Randomat:IsTraitorTeam() then
-                -- Turn them into phantoms
-                Randomat:SetRole(ply, ROLE_PHANTOM)
-            end
+    -- For all alive players,
+    for k, ply in pairs(self:GetAlivePlayers(true)) do
+        -- That aren't traitors
+        if not Randomat:IsTraitorTeam() then
+            -- Turn them into phantoms
+            Randomat:SetRole(ply, ROLE_PHANTOM)
         end
     end
 
@@ -26,14 +18,8 @@ function EVENT:Begin()
 end
 
 function EVENT:Condition()
-    local isPhantom = false
-
     -- If the phantom role's enumerator exists and isn't -1, the role exists and this randomat can trigger
-    if isnumber(ROLE_PHANTOM) and ROLE_PHANTOM ~= -1 then
-        isPhantom = true
-    end
-
-    return isPhantom
+    return isnumber(ROLE_PHANTOM) and ROLE_PHANTOM ~= -1
 end
 
 Randomat:register(EVENT)

be0af4f1f3145d2fa8476ade034afa950b303ec9
The Stig <TheStig294@users.noreply.github.com>
2021-06-15 18:04:53 +0930

    Cleanup and added comments

--- lua/randomat2/events/teambuilding.lua
+++ lua/randomat2/events/teambuilding.lua
@@ -7,32 +7,38 @@ function EVENT:Begin()
     local tx = 0
     local dx = 0
 
+    -- For all alive players,
     for _, ply in pairs(self:GetAlivePlayers(true)) do
+        -- If the credits and health of the traitor and detective haven't been set yet, and they're a detective or a traitor,
         if (ply:GetRole() == ROLE_TRAITOR and tx == 0) or (ply:GetRole() == ROLE_DETECTIVE and dx == 0) then
+            -- If they're not the detective, (and thus the traitor)
             if ply:GetRole() ~= ROLE_DETECTIVE then
+                -- Say the traitor's credits have been changed and set them to 2
                 tx = 1
-                ply:SetMaxHealth(100)
+                ply:SetCredits(2)
             else
+                -- Else, they must be the detective, and change their credits and health
                 dx = 1
                 ply:SetCredits(2)
-                ply:SetHealth(150)
-                ply:SetMaxHealth(150)
+                -- Detective is set to 200 health to compensate for the traitor knowing who the detective is but not the other way around
+                ply:SetHealth(200)
+                ply:SetMaxHealth(200)
             end
         else
+            -- Set everyone else to beggars
             Randomat:SetRole(ply, ROLE_BEGGAR)
-            ply:SetCredits(0)
-            ply:SetMaxHealth(100)
 
+            -- And remove any bought weapons or role weapons
             for _, wep in pairs(ply:GetWeapons()) do
-                if wep.Kind == WEAPON_EQUIP1 or wep.Kind == WEAPON_EQUIP2 then
+                -- Checking this way instead of wep.Kind ~= WEAPON_ROLE or wep.Kind ~= WEAPON_EQUIP for compatibility with my slot removal mod
+                if wep.Kind ~= WEAPON_PISTOL or wep.Kind ~= WEAPON_HEAVY or wep.Kind ~= WEAPON_NADE or wep.Kind ~= WEAPON_MELEE or wep.Kind ~= WEAPON_CARRY or wep.Kind ~= WEAPON_UNARMED then
                     ply:StripWeapon(wep:GetClass())
                 end
             end
-
-            self:StripRoleWeapons(ply)
         end
     end
 
+    -- Let end-of-round report know roles have changed
     SendFullStateUpdate()
 end
 
@@ -41,10 +47,12 @@ function EVENT:Condition()
     local has_traitor = false
     local has_beggar = false
 
+    -- Check if the beggar exists and is enabled
     if ConVarExists("ttt_beggar_enabled") and GetConVar("ttt_beggar_enabled"):GetBool() then
         has_beggar = true
     end
 
+    -- Check if there is a detective and a traitor in the round
     for _, v in pairs(self:GetAlivePlayers()) do
         if v:GetRole() == ROLE_DETECTIVE then
             has_detective = true
@@ -52,6 +60,7 @@ function EVENT:Condition()
             has_traitor = true
         end
     end
+    -- Let this randomat trigger if there is a traitor and detective and the beggar role is enabled
 
     return has_traitor and has_detective and has_beggar
 end

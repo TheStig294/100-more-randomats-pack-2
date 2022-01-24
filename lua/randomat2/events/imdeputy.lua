72db365010b74ed0f261ba01f29e80300f566e71
The Stig <TheStig294@users.noreply.github.com>
2022-01-24 15:17:00 +1030

    Removed now redundant checks to promote players

--- lua/randomat2/events/imdeputy.lua
+++ lua/randomat2/events/imdeputy.lua
@@ -4,43 +4,13 @@ EVENT.Description = "Pure innocents are now deputies, traitors are now impersona
 EVENT.id = "imdeputy"
 
 function EVENT:Begin()
-    local isDetective = false
-
-    for i, ply in ipairs(self:GetAlivePlayers()) do
-        if Randomat:IsGoodDetectiveLike(ply) then
-            isDetective = true
-        end
-    end
-
-    local promoteDeputies = false
-    local promoteImpersonators = false
-
-    if not isDetective and ConVarExists("ttt_deputy_without_detective") and GetConVar("ttt_deputy_without_detective"):GetBool() then
-        promoteDeputies = true
-    end
-
-    if not isDetective and ConVarExists("ttt_impersonator_without_detective") and GetConVar("ttt_impersonator_without_detective"):GetBool() then
-        promoteImpersonators = true
-    end
-
-    -- If there is a clown or jester already then there is no need to turn someone into one
     for k, ply in pairs(self:GetAlivePlayers()) do
         if ply:GetRole() == ROLE_INNOCENT then
             self:StripRoleWeapons(ply)
             Randomat:SetRole(ply, ROLE_DEPUTY)
-
-            if promoteDeputies then
-                ply:SetNWBool("HasPromotion", true)
-                ply:AddCredits(GetConVar("ttt_deputy_activation_credits"):GetInt())
-            end
         elseif Randomat:IsTraitorTeam(ply) then
             self:StripRoleWeapons(ply)
             Randomat:SetRole(ply, ROLE_IMPERSONATOR)
-
-            if promoteImpersonators then
-                ply:SetNWBool("HasPromotion", true)
-                ply:AddCredits(GetConVar("ttt_impersonator_activation_credits"):GetInt())
-            end
         end
     end
 

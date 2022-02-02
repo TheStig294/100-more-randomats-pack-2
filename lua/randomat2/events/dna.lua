4a94d6ca5a04b2889649c37504921304ab3f32b3
The Stig <TheStig294@users.noreply.github.com>
2022-02-02 22:26:07 +1030

    Added chat message to use the DNA scanner when looking at a body for the first time

--- lua/randomat2/events/dna.lua
+++ lua/randomat2/events/dna.lua
@@ -14,11 +14,41 @@ function EVENT:Begin()
     end
 
     GetConVar("ttt_detective_search_only"):SetBool(false)
+    SetGlobalBool("ttt_detective_search_only", false)
+
+    -- Adds a message to chat when a player looks at a body while they have a DNA scanner to use it
+    self:AddHook("TTTOnCorpseCreated", function(corpse)
+        corpse.IsBody = true
+    end)
+
+    local nonTraitors = {}
+
+    for i, ply in ipairs(self:GetAlivePlayers()) do
+        if not Randomat:IsTraitorTeam(ply) then
+            table.insert(nonTraitors, ply)
+        end
+    end
+
+    timer.Create("RandomatDNAScannerBodyCheck", 0.5, 0, function()
+        for i, ply in ipairs(nonTraitors) do
+            local ent = ply:GetEyeTrace().Entity
+
+            if IsValid(ent) and ent.IsBody and ply:HasWeapon("weapon_ttt_wtester") and not ply:GetNWBool("DNARandomatSeenBody") then
+                ply:ChatPrint("Left-click with the DNA scanner on the body!")
+                ply:SetNWBool("DNARandomatSeenBody", true)
+            end
+        end
+    end)
 end
 
 function EVENT:End()
     if eventTriggered then
         GetConVar("ttt_detective_search_only"):SetBool(true)
+        timer.Remove("RandomatDNAScannerBodyCheck")
+
+        for i, ply in ipairs(player.GetAll()) do
+            ply:SetNWBool("DNARandomatSeenBody", false)
+        end
     end
 end
 

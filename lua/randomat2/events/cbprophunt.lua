9052aa90679a4866b2f4ef8a5d7fb2127ef4a5e7
The Stig <TheStig294@users.noreply.github.com>
2022-10-02 09:44:00 +1030

    Fixed not respawning players

--- lua/randomat2/events/cbprophunt.lua
+++ lua/randomat2/events/cbprophunt.lua
@@ -6,16 +6,24 @@ EVENT.id = "cbprophunt"
 EVENT.Categories = {"gamemode", "eventtrigger", "rolechange", "largeimpact"}
 
 local event1 = "prophunt"
-local event2 = "morality"
 
 function EVENT:Begin()
     Randomat:SilentTriggerEvent(event1)
-    Randomat:SilentTriggerEvent(event2)
+
+    self:AddHook("DoPlayerDeath", function(ply, attacker, dmg)
+        if not Randomat:IsTraitorTeam(ply) then
+            ply:SpawnForRound(true)
+
+            timer.Simple(1, function()
+                Randomat:SetRole(ply, ROLE_TRAITOR)
+            end)
+        end
+    end)
 end
 
 -- Don't run if props joining hunters is a thing anyway with the prop hunt randomat
 function EVENT:Condition()
-    return Randomat:CanEventRun(event1) and Randomat:CanEventRun(event2) and ConVarExists("randomat_prophunt_props_join_hunters") and not GetConVar("randomat_prophunt_props_join_hunters"):GetBool()
+    return Randomat:CanEventRun(event1) and ConVarExists("randomat_prophunt_props_join_hunters") and not GetConVar("randomat_prophunt_props_join_hunters"):GetBool()
 end
 
 Randomat:register(EVENT)

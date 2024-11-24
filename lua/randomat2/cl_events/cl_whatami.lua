8a1298e4b6a5ca397273016af5b4e645e83914d6
thestig294 <thestig294@gmail.com>
2024-11-24 23:55:41 +1030

    Code cleanup for "What am I?" randomat

--- lua/randomat2/cl_events/cl_whatami.lua
+++ lua/randomat2/cl_events/cl_whatami.lua
@@ -1,47 +1,29 @@
 -- Default role hint popup time is 17 seconds
 local popupTime = 17
 
--- Sent whenever this randomat tries to trigger
--- Checks if the 'Hide role' option exists
--- If not this randomat does not trigger
 net.Receive("WhatAmIRandomatConVarCheck", function()
-    -- If Malivil's 'Hide role' setting is detected,
-    if ConVarExists("ttt_hide_role") then
-        -- Then tell the server this randomat can trigger,
-        net.Start("WhatAmIRandomatCondition")
-        net.WriteBool(true)
-        net.SendToServer()
-    else
-        -- Otherwise it cannot
-        net.Start("WhatAmIRandomatCondition")
-        net.WriteBool(false)
-        net.SendToServer()
-    end
+    local hideRoleCvarExists = ConVarExists("ttt_hide_role")
+    net.Start("WhatAmIRandomatCondition")
+    net.WriteBool(hideRoleCvarExists)
+    net.SendToServer()
 end)
 
 local function EndWhatAmI()
     cvars.RemoveChangeCallback("ttt_hide_role", "WhatAmIPreventUnhide")
-    -- Resets the role hint popup time
     GetConVar("ttt_startpopup_duration"):SetInt(popupTime)
-    -- Un-hides the player's role
     GetConVar("ttt_hide_role"):SetBool(false)
-    -- Re-enables the scoreboard and weapon pickup popups
-    hook.Remove("PlayerBindPress", "WhatAmIDisableScorboard")
+    hook.Remove("PlayerBindPress", "WhatAmIDisableScoreboard")
     hook.Remove("HUDShouldDraw", "WhatAmIDisableWeaponHistory")
     hook.Remove("TTTBeginRound", "ForceEndWhatAmIBeginRound")
     hook.Remove("TTTEndRound", "ForceEndWhatAmIEndRound")
     hook.Remove("ShutDown", "ForceEndWhatAmIEndRound")
 end
 
--- Randomat start
 net.Receive("WhatAmIRandomat", function()
-    -- Get the role hint popup time and turn it off
     popupTime = GetConVar("ttt_startpopup_duration"):GetInt()
     GetConVar("ttt_startpopup_duration"):SetInt(0)
-    -- Hide the player's role
     GetConVar("ttt_hide_role"):SetBool(true)
 
-    -- Prevent the players from un-hiding their role by unchecking the "hide role" tickbox in the F1 menu
     local function WhatAmIPreventUnhide(convar, oldValue, newValue)
         if convar == "ttt_hide_role" then
             GetConVar("ttt_hide_role"):SetBool(true)
@@ -50,12 +32,10 @@ net.Receive("WhatAmIRandomat", function()
 
     cvars.AddChangeCallback("ttt_hide_role", WhatAmIPreventUnhide, "WhatAmIPreventUnhide")
 
-    -- Prevent them from opening the scoreboard
-    hook.Add("PlayerBindPress", "WhatAmIDisableScorboard", function(ply, bind, pressed)
-        if (string.find(bind, "+showscores")) then return true end
+    hook.Add("PlayerBindPress", "WhatAmIDisableScoreboard", function(ply, bind, pressed)
+        if string.find(bind, "+showscores") then return true end
     end)
 
-    -- Hides the weapon pickup popup on the side as it's colour gives away your role
     hook.Add("HUDShouldDraw", "WhatAmIDisableWeaponHistory", function(name)
         if name == "TTTPickupHistory" then return false end
     end)
--- lua/randomat2/events/whatami.lua
+++ lua/randomat2/events/whatami.lua
@@ -6,41 +6,36 @@ EVENT.id = "whatami"
 EVENT.Categories = {"largeimpact"}
 
 util.AddNetworkString("WhatAmIRandomat")
-util.AddNetworkString("WhatAmIRandomatEnd")
 util.AddNetworkString("WhatAmIRandomatConVarCheck")
 util.AddNetworkString("WhatAmIRandomatCondition")
-local ConVarCheckDone = false
-local EventRun = false
--- Variable doesn't work unless it's global
-whatAmIRandomatHideRole = false
+local hideRoleCvarExists = false
 
--- At the start of the first round,
 hook.Add("TTTPrepareRound", "WhatAmIRandomatConVarCheckHook", function()
-    -- If the convar check hasn't already been done,
-    if ConVarCheckDone == false then
-        -- Check if the 'Hide Role' Convar exists on the first connected player's client
+    -- Entity(1) always returns the first connected player,
+    -- but on dedicated servers there isn't always a guarantee that there is a player on the server
+    local firstPly = Entity(1)
+
+    if IsValid(firstPly) then
         net.Start("WhatAmIRandomatConVarCheck")
-        net.Send(Entity(1))
-        -- Prevent this from happening every round
-        ConVarCheckDone = true
+        net.Send(firstPly)
+        hook.Remove("TTTPrepareRound", "WhatAmIRandomatConVarCheckHook")
     end
 end)
 
--- Receive from the client whether the hide role convar exists
 net.Receive("WhatAmIRandomatCondition", function()
-    whatAmIRandomatHideRole = net.ReadBool()
+    hideRoleCvarExists = net.ReadBool()
 end)
 
+local eventRunBefore = false
+
 function EVENT:Begin()
-    -- The action happens client-side
     net.Start("WhatAmIRandomat")
     net.Broadcast()
-    EventRun = true
+    eventRunBefore = true
 end
 
 function EVENT:Condition()
-    -- Don't let this randomat run if the convar doesn't exist or it has run before for this map
-    return whatAmIRandomatHideRole and not EventRun
+    return hideRoleCvarExists and not eventRunBefore
 end
 
 Randomat:register(EVENT)

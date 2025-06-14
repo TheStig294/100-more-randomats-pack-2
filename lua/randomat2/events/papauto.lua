ef2d8be025a2c99b6108231db60581f92145969c
The Stig <thestig294@gmail.com>
2025-06-14 17:02:58 +0930

    Updated "Everything's Upgraded!" for PaP update

--- lua/randomat2/events/papauto.lua
+++ lua/randomat2/events/papauto.lua
@@ -7,23 +7,13 @@ EVENT.Categories = {"largeimpact", "item"}
 
 function EVENT:Begin()
     timer.Create("AutoPaPRandomat", 1, 0, function()
-        for _, ent in ipairs(ents.GetAll()) do
+        for _, ent in ents.Iterator() do
             -- Check for a valid entity, if the entity is a weapon, if the entity is a TTT weapon, and not the "holstered" weapon
             -- Don't try to upgrade weapons more than once, as a weapon could error from being upgraded off the ground, and block this for loop from looping though all weapons
             if IsValid(ent) and not ent.PAPAutoUpgrade and ent.Kind and ent:IsWeapon() and WEPS.GetClass(ent) ~= "weapon_ttt_unarmed" then
                 ent.PAPAutoUpgrade = true
                 TTTPAP:ApplyRandomUpgrade(ent)
             end
-        end
-    end)
-
-    self:AddHook("PlayerSwitchWeapon", function(ply, oldSWEP, newSWEP)
-        if IsValid(newSWEP) and newSWEP.PAPUpgrade and newSWEP.PAPUpgrade.desc then
-            if not IsValid(newSWEP.LastPlayerSwitchedTo) or newSWEP.LastPlayerSwitchedTo ~= ply then
-                ply:ChatPrint("PAP UPGRADE: " .. newSWEP.PAPUpgrade.desc)
-            end
-
-            newSWEP.LastPlayerSwitchedTo = ply
         end
     end)
 end
